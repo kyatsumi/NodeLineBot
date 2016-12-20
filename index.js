620b1d6c275dce7fe72da419843595d1457ce824
katsumi <k.m.penkoka@gmail.com>
2016-12-20 22:35:43 +0900

    added post method

--- index.js
+++ index.js
@@ -4,15 +4,24 @@
  * and open the template in the editor.
  */
 
+//モジュールのインポート
 var express = require('express');
 var app = express();
 
+//Webサーバー設定
 var port = (process.env.PORT || 3000);
 var server = app.listen(port, function() {
    console.log('Node is running on port ' + port);
 });
 
+//ルーターの設定
+//GET
 app.get('/', function(req, res, next) {
-   res.send('Node is running on port ' + port); 
    res.send('Node is running on port ' + port);
+});
+
+//POST
+app.post('/callback', function(req, res, next) {
+    res.status(200).end();
+    console.log(req.body);
 });
