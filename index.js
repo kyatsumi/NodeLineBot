6f15682646329f3eda02c4680a2852819fc4977c
katsumi <k.m.penkoka@gmail.com>
2016-12-23 19:50:59 +0900

    add apiai

--- index.js
+++ index.js
@@ -2,25 +2,27 @@
  * Line Message APIのWebhookを受ける
  */
 
-/* Heroku環境からアクセストークンを取得 */
-const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
-/* ラインのリプライ用URL*/
-const LINE_REQUEST_POST = 'https://api.line.me/v2/bot/message/reply';
-/* ラインのリクエストヘッダー */
-const LINE_REQUEST_HEADERS = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
-            };
+
+
+/* Heroku環境からAIP.aiアクセストークンを取得 */
+const APIAI_CLIENT_ACCESS_TOKEN = process.env.APIAI_CLIENT_ACCESS_TOKEN;
+
 
 //モジュールのインポート
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request');
+var apiai = require('apiai');
+var uuid = require('node-uuid');
+var Promise = require('bluebird');
 var signatureValidation = require('./signatureValidation');
+var reply = require('./reply');
 var app = express();
 
 //ミドルウェア設定
 app.use(bodyParser.json());
+Promise.confi({
+    cancellation: true
+});
 
 //Webサーバー設定
 var port = (process.env.PORT || 3000);
@@ -42,21 +44,44 @@ app.post('/callback', function(req, res, next) {
         return;
     } 
     for (var event of req.body.events) {
-        if (event.type == 'message') {
-            var body = {
-                replyToken : event.replyToken,
-                messages: [{
-                    type: 'text',
-                    text: event.message.text
-                }]
-            };
-            request({
-                url: LINE_REQUEST_POST,
-                method: 'POST',
-                headers: LINE_REQUEST_HEADERS,
-                body: body,
-                json: true
+        if (event.type == 'message' && event.message.text) {
+            var aiInstance = apiai(APIAI_CLIENT_ACCESS_TOKEN);
+            var aiRequest = aiInstance.textRequest(event.message.text, {sessionId: uuid.v1()});
+            var gotIntent = new Promise(function(resolve, reject){
+                aiRequest.on('response', function(response){
+                    resolve(response);
+                });
+                aiRequest.end();
             });
+            gotIntent.then(function(response) {
+                console.log(response.result.action);
+                switch (response.result.action) {
+                        case 'shopLocation':
+                            var body = {
+                                replyToken : event.replyToken,
+                                messages: [{
+                                    type: 'location',
+                                    title: '架空のお店です',
+                                    address: '名古屋駅のそのへん',
+                                    latitude: '35.170983',
+                                    longitude: '136.882874'
+                                }]
+                            };
+                            reply.replyTextMessage(body);
+                            break;
+                        default:
+                            var body = {
+                                replyToken : event.replyToken,
+                                messages: [{
+                                    type: 'text',
+                                    text: event.message.text
+                                }]
+                            };
+                            reply.replyTextMessage(body);
+                            break;
+}
+            });
+
         }
     }
 });
