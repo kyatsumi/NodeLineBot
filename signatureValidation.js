4be10b3813fb2055d25ccba473d0b4df2dddda10
katsumi <k.m.penkoka@gmail.com>
2016-12-23 14:14:51 +0900

    move crypto

--- signatureValidation.js
+++ signatureValidation.js
@@ -3,7 +3,8 @@
  */
 
 
-var signatureValidation = function(signature, body, crypto) {
+var signatureValidation = function(signature, body) {
+    var crypto = require('crypto');
     /* Heroku環境から秘密鍵を取得 */
     const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
     return signature == crypto.createHmac('sha256', LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
