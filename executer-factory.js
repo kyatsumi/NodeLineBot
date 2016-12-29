38c61ba9a00c287e45c70458bc95abd1414be2a1
katsumi <k.m.penkoka@gmail.com>
2016-12-29 21:39:54 +0900

    change ai api.ai → luis

--- executer-factory.js
+++ executer-factory.js
@@ -2,19 +2,12 @@
 /* 環境からAIP.aiアクセストークンを取得 */
 const APIAI_CLIENT_ACCESS_TOKEN = process.env.APIAI_CLIENT_ACCESS_TOKEN;
 
-//AIP.ai用のSDK
-var apiai = require('apiai');
-//セッション管理用uuid生成モジュール
-var uuid = require('node-uuid');
+var luis = request('./luis');
 
 /**
  * Promise用のExecuterを生成
  * Excuter実行後のレスポンスとしてAIに登録してある文字列を返す
  * "お店に行きたい"と同じ意味の言葉はshopLocationという単語に変換されるように登録してある。
- * 例：お店に行きたい　→　shopLocation
- * 　　お店の地図　　　→　shopLocation
- * 　　みせの場所　　　→　shopLocation
- *     メニュー       →  undefined
  * @param {type} message 
  * @returns {nm$_executer-factory.module.exports.executerByMessage.apiaiExecutor|
  * nm$_executer-factory.module.exports.executerByMessage.executor} 
@@ -23,15 +16,11 @@ var uuid = require('node-uuid');
 module.exports.executerByMessage = function(message) {
     switch(message.type) {
         case 'text':
-            var aiInstance = apiai(APIAI_CLIENT_ACCESS_TOKEN);
-            var aiRequest = aiInstance.textRequest(message.text, {sessionId: uuid.v1()});
-            var apiaiExecutor = function(resolve, reject) {
-                aiRequest.on('response', function(response){
-                    resolve(response.result.action);
-                });
-                aiRequest.end();
+            luis(message.text);
+            var executor = function(resolve, reject) {
+                resolve('empty');
             };
-            return apiaiExecutor;
+            return executor;
         default:
             var executor = function(resolve, reject) {
                 resolve('empty');
---
+++ luis.js
@@ -0,0 +1,16 @@
+/* 
+ */
+
+var request = require('request');
+
+var luisUrl = 'https://api.projectoxford.ai/luis/v2.0/apps/04873a90-bfb6-4e20-9fe7-1cf73c7cf919?subscription-key=080de2aa58ea4010a090de37b8e2784d&q=';
+
+module.exports = function(text) {
+    request(luisUrl + text, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(JSON.parse(body));
+  } else {
+    console.log('error: '+ response.statusCode);
+  }
+});
+};
