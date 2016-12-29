2486541c6fd00cfbbec336c8b171c130b8571d10
katsumi <k.m.penkoka@gmail.com>
2016-12-29 22:23:24 +0900

    luis test2

--- luis.js
+++ luis.js
@@ -3,13 +3,15 @@
 
 var request = require('request');
 
-var luisUrl = 'https://api.projectoxford.ai/luis/v2.0/apps/04873a90-bfb6-4e20-9fe7-1cf73c7cf919?subscription-key=080de2aa58ea4010a090de37b8e2784d&q=%E3%81%8A%E5%BA%97%E3%81%AE%E5%A0%B4%E6%89%80';
+var options = {
+    url: 'https://api.projectoxford.ai/luis/v2.0/apps/04873a90-bfb6-4e20-9fe7-1cf73c7cf919?subscription-key=080de2aa58ea4010a090de37b8e2784d&q=%E3%81%8A%E5%BA%97%E3%81%AE%E5%A0%B4%E6%89%80',
+    json: true
+};
 
 module.exports = function(text) {
-    var intent = request(luisUrl, function (error, response, body) {
+    var intent = request.get(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          json = JSON.parse(body);
-          return json.topScoringIntent.intent;
+          return body.topScoringIntent.intent;
         } else {
           console.log('error: '+ response.statusCode);
           return 'undefined';
